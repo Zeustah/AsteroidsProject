df8af2aa11ea6fbbcebce1f5a7511022e638b5b8
Zeustah <ar8358@gmail.com>
2025-11-04 09:29:04 -0500

    (3) added 60 FPS cap

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:  # Game Loop
         for event in pygame.event.get():
@@ -11,6 +13,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        FPS = clock.tick(60)
+        dt = FPS / 1000
 
     print("Starting Asteroids!")
     print("Screen width: 1280")
