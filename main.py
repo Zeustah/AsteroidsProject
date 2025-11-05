1a5bffcf99caed067e483757a4b3217f2d6d0ecb
Zeustah <ar8358@gmail.com>
2025-11-05 09:40:57 -0500

    (8) added asteroid field and asteroid models/movement

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
+from asteroidfield import AsteroidField
 from constants import *
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = updatable
 
 
 def main():
@@ -14,6 +19,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_unit = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     while True:  # Game Loop
         for event in pygame.event.get():
