b990bb9f54c17dcd6d1368129d73efa61eb040ec
Zeustah <ar8358@gmail.com>
2025-11-05 07:57:36 -0500

    (7) set player model as pygame group/container

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from constants import *
 from circleshape import CircleShape
 from player import Player
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     player_unit = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:  # Game Loop
@@ -17,8 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player_unit.update(dt)
-        player_unit.draw(screen)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         FPS = clock.tick(60)
         dt = FPS / 1000
